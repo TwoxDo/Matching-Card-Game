d343570a574ba831c3eaa27f20bf9e745d2ac0d6
Mostafa Mohamed <twoxdo@gmail.com>
2022-08-18 01:33:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,14 +15,6 @@ const cardImages = [
  {"src": "/img2/tiger.jpeg",matching:false}
 
 ]
-// const cardImages = [
-//   { "src": "/img/helmet-1.png", matched: false },
-//   { "src": "/img/potion-1.png", matched: false },
-//   { "src": "/img/ring-1.png", matched: false },
-//   { "src": "/img/scroll-1.png", matched: false },
-//   { "src": "/img/shield-1.png", matched: false },
-//   { "src": "/img/sword-1.png", matched: false },
-// ]
 
 
 function App() {
